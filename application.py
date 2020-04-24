3553c6fa6bf2d484b7adf2a86f196a936b772e7c
Prashambhuta <prasham.bhuta@gmail.com>
2020-04-24 22:37:59 +0530

    Adding links, about & resume

--- application.py
+++ application.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
